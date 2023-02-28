a8dd6b734ac4552a9b05c4d5df8f6f9231db89fd
rotimi <rotimi@localhost>
2023-02-27 18:41:44 -0700

    Setup in progress

--- benchmark-code/ubench-benchmarks/LeanOrmHasManyOrHasManyThroughRunner.php
+++ benchmark-code/ubench-benchmarks/LeanOrmHasManyOrHasManyThroughRunner.php
@@ -23,8 +23,7 @@ class LeanOrmHasManyOrHasManyThroughRunner {
      * 
      * @param int $offset           Offset position
      * 
-     * @param int $limit            Number of records to fetch per iteration
-     *                              We need this for sqlite.
+     * @param int $limit            Number of records to fetch per iteration.
      */
     public function __invoke(
         \Ubench $ubench,
