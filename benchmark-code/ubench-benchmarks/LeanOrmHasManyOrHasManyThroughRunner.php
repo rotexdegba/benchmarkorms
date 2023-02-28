c23a346f72cea20870ed79378e966167a4eaf767
rotimi <rotimi@localhost>
2023-02-28 01:34:24 -0700

    Setup in progress

--- benchmark-code/ubench-benchmarks/LeanOrmHasManyOrHasManyThroughRunner.php
+++ benchmark-code/ubench-benchmarks/LeanOrmHasManyOrHasManyThroughRunner.php
@@ -1,6 +1,8 @@
 <?php
 namespace Rotexsoft\PhpOrmBenchmarks\Ubench;
 
+use \Rotexsoft\PhpOrmBenchmarks\LeanOrm\{LeanOrmDataFetcher, LeanOrmFetchStrategies};
+
 /**
  * Description of LeanOrmHasManyOrHasManyThroughRunner
  *
@@ -13,63 +15,86 @@ class LeanOrmHasManyOrHasManyThroughRunner {
     /**
      * @param \Ubench $ubench       Ubench instance
      * 
-     * @param \LeanOrm\Model $lean  Model for fetching records
+     * @param string $relation_names Relation name (has many or has many through)
      * 
-     * @param string $relation_name Relation name (has many or has many through)
-     * 
-     * @param string $property_name A property on the records to be fetched. 
+     * @param string $table_column_name A property on the records to be fetched. 
      *                              For example if we are fetching authors 
      *                              we can specify name for this argument
      * 
      * @param int $offset           Offset position
      * 
-     * @param int $limit            Number of records to fetch per iteration.
+     * @param int $limit            Number of records to fetch per iteration
      */
     public function __invoke(
         \Ubench $ubench,
-        \LeanOrm\Model $lean,
-        string $relation_name,
-        string $property_name,
+        string $table_name, 
+        array $relation_names,
+        string $table_column_name,
+        string $relation_column_name,
         int $offset = 0,
-        int $limit = 999
+        int $limit = 999,
+        string $strategy= LeanOrmFetchStrategies::FETCH_ROWS_INTO_ARRAY,
+        array $pdo_args =[]
     ) {
+        $num_records = 0;
+        
+        \Rotexsoft\PhpOrmBenchmarks\Utils::showOrmVersion(MessageResources::PACKAGIST_NAME_LEAN);
+        
+        \Rotexsoft\PhpOrmBenchmarks\Utils::printDbInfo(
+            LeanOrmDataFetcher::getModel($table_name, $pdo_args)->getPDO()
+        );
+        
+        echo sprintf(
+            MessageResources::START_MSG, MessageResources::ORM_VENDOR_LEAN, $table_name, implode(', ', $relation_names), 
+            MessageResources::HAS_MANY_OR_HMT, $limit, $strategy, $table_column_name, $table_name
+        );
+        
         $ubench->run(
             function(
-                \LeanOrm\Model $lean, 
                 $offset, 
-                $limit, 
-                $relation_name, 
-                $property_name
-            ) {
-                $i = 1;
-
+                $limit,
+                $table_name,
+                $relation_names, 
+                $table_column_name,
+                $relation_column_name,
+                $strategy,
+                $pdo_args
+            ) use (&$num_records) {
                 do {
-                    $recordSet = $lean->fetchRecordsIntoCollection(
-                        $lean->getSelect()
-                             ->limit($limit)
-                             ->offset($offset)
-                        ,
-                        [$relation_name]
-                    );
+                    $recordSet = LeanOrmDataFetcher::fetchAll($table_name, $relation_names, $offset, $limit, $strategy, $pdo_args);
 
                     foreach ($recordSet as $record) {
 
-                        $val = $record[$property_name];
-                        $count = $record->$relation_name->count();
-                        //var_dump("{$val} {$i}");
-                        //var_dump("{$val} {$i} with {$record->$relation_name->count()} {$relation_name}");
-                        $i++;
+                        $val = $record[$table_column_name];
+                        
+                        foreach($relation_names as $relation_name) {
+                            
+                            foreach ($record[$relation_name] as $related_record) {
+                                
+                                $related_record[$relation_column_name];
+                            }
+                        }
+                        
+                        $num_records++; //var_dump("{$num_records} {$val}");
                     }
 
                     $offset += $limit;
                     
                 }while(count($recordSet) > 0);
             },
-            $lean,
             $offset, 
             $limit,
-            $relation_name,
-            $property_name
+            $table_name,
+            $relation_names,
+            $table_column_name,
+            $relation_column_name,
+            $strategy,
+            $pdo_args
+        );
+        
+        echo sprintf(
+            MessageResources::END_MSG, $table_name, $num_records, 
+            $ubench->getTime(), $ubench->getMemoryUsage(), $ubench->getMemoryPeak()
         );
     }
 }
