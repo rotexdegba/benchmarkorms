a9029a16615dab04903c111f0262b4c246068d71
rotimi <rotimi@localhost>
2023-04-01 11:57:36 -0600

    Added Eager loading benchmarks

---
+++ benchmark-scenarios/fetch-first-n-rows-with-eager-loaded-related-data/with-belongs-to-and-has-one-data/atlas-fetch-first-500-posts-with-belongs-to-and-has-one-related-data-using-fetchRecords.php
@@ -0,0 +1,20 @@
+<?php
+include __DIR__ .DIRECTORY_SEPARATOR. '../../../load-atlas.php';
+
+$scenario = ucfirst(str_replace( ['-','.php'], [' ', ''], basename(__FILE__) ));
+
+echo \Rotexsoft\PhpOrmBenchmarks\Ubench\MessageResources::RUNNING_SCENARIO 
+    . $scenario . PHP_EOL . PHP_EOL;
+
+$runner = new \Rotexsoft\PhpOrmBenchmarks\Ubench\AtlasBelongsToOrHasOneRunner();
+
+$runner(
+    new Ubench(), $atlas, 'posts', [ 'author' => 'name', 'summary' => 'post_id' ], 'title', 0, 500,
+    \Rotexsoft\PhpOrmBenchmarks\AtlasOrm\AtlasFetchStrategies::FETCH_RECORDS,
+    $_SERVER['argv'][1], true
+);
+
+echo \Rotexsoft\PhpOrmBenchmarks\Ubench\MessageResources::SCENARIO_ENDED
+    . $scenario . PHP_EOL 
+    . str_repeat('=========================================================', 2)
+    . PHP_EOL . PHP_EOL;
---
+++ benchmark-scenarios/fetch-first-n-rows-with-eager-loaded-related-data/with-belongs-to-and-has-one-data/eloquent-fetch-first-100-comments-with-belongs-to-and-has-one-related-data-using-get.php
@@ -0,0 +1,20 @@
+<?php
+include __DIR__ .DIRECTORY_SEPARATOR. '../../../load-eloquent.php';
+
+$scenario = ucfirst(str_replace( ['-','.php'], [' ', ''], basename(__FILE__) ));
+
+echo \Rotexsoft\PhpOrmBenchmarks\Ubench\MessageResources::RUNNING_SCENARIO 
+    . $scenario . PHP_EOL . PHP_EOL;
+
+$runner = new \Rotexsoft\PhpOrmBenchmarks\Ubench\EloquentBelongsToOrHasOneRunner();
+
+$runner(
+    new Ubench(), 'comments', [ 'post' => 'title'], 'name', 0, 100,
+    \Rotexsoft\PhpOrmBenchmarks\Eloquent\EloquentFetchStrategies::GET,
+    $_SERVER['argv'][1], true
+);
+
+echo \Rotexsoft\PhpOrmBenchmarks\Ubench\MessageResources::SCENARIO_ENDED
+    . $scenario . PHP_EOL 
+    . str_repeat('=========================================================', 2)
+    . PHP_EOL . PHP_EOL;
