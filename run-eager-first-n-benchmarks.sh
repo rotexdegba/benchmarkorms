ca73d5f1bd27abd75ce386f3c7313aed8b46ff4d
rotimi <rotimi@localhost>
2023-04-01 12:09:51 -0600

    Added Eager loading benchmarks

---
+++ run-eager-first-n-benchmarks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if(($# < 1))
+then
+    echo "You must specify the path where results will be saved"
+    exit
+fi
+
+# $1 should be the path to a folder / directory where the results will be saved
+
+date_now=$(date)
+
+header=$(php -r "include __DIR__ . '/vendor/autoload.php'; echo \Rotexsoft\PhpOrmBenchmarks\Ubench\MessageResources::BENCHMARK_RESULT_EAGER_DATA_HEADER;")
+
+php ./benchmark-scenarios/welcome.php "$date_now"
+
+source ./eager-loading-first-n-benchmarks.sh  "$date_now"
+
+php ./benchmark-scenarios/goodbye.php "$date_now" "$1" "LATEST_RESULTS_FIRST_N_EAGER.md" "LATEST_RESULTS_FIRST_N_EAGER.html" "$header"
